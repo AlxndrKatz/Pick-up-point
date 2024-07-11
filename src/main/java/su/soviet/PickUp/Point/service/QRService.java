e57337fb488f0b117da74a259afffe5601ae882e
aleksandr_katsur <alexander_katzur@mail.ru>
2024-07-11 11:26:42 +0700

    Смена логики формирования и обработки QR.

--- src/main/java/su/soviet/PickUp/Point/service/QRService.java
+++ src/main/java/su/soviet/PickUp/Point/service/QRService.java
@@ -5,6 +5,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -13,8 +14,12 @@ import java.io.IOException;
 @Service
 public class QRService {
 
+    @Value("${app.host}")
+    private String host;
+
     public byte[] generateQRCode(Long userId, int width, int height) throws WriterException, IOException {
-        String link = "http://localhost:8080/pick-up-point/employee/get-my-orders/?userId=" + userId;
+
+        String link = host.concat("/pick-up-point/employee/get-my-orders/?userId=" + userId);
         QRCodeWriter qrWriter = new QRCodeWriter();
         BitMatrix matrix = qrWriter.encode(link, BarcodeFormat.QR_CODE, width, height);
 
